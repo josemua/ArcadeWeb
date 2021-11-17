1b42c1bb777edbb51fba174cd26be44c94bce21b
Fengrand <collerx100pre@gmail.com>
2021-11-16 20:09:12 -0500

    primer login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
+import { LoginButton } from './Componentes/Login';
+import { Profile } from './Componentes/Profile';
+import { LogoutButton } from './Componentes/Logout';
 
 function App() {
+  const {isAuthenticated} = useAuth0();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          {/* condicion para intercalar boton dependiendo si esta logeado o no */}
+          { isAuthenticated ? (
+            <> 
+            <LogoutButton />
+            <Profile />
+            </>
+          )  : (
+          <LoginButton />
+          )}
+        </header>
+      </div>
   );
 }
 
---
+++ src/Componentes/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import '../css/bootstrap.min.css';
+
+export const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+
+    return <button onClick={() => loginWithRedirect()} className="btn btn-primary">Login</button>
+};
